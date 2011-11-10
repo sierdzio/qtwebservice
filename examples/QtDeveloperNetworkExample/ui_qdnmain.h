ad42cb85080b2154bd6753fdd1b1926d933f1783
Tomasz Siekierda (sierdzio) <sierdzio@gmail.com>
2011-11-10 15:21:22 +0100

    Add tutorial 4 - using QtWebService in QML.

--- examples/QtDeveloperNetworkExample/ui_qdnmain.h
+++ examples/QtDeveloperNetworkExample/ui_qdnmain.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'qdnmain.ui'
 **
-** Created: Wed Nov 9 12:16:00 2011
+** Created: Thu Nov 10 15:20:53 2011
 **      by: Qt User Interface Compiler version 4.7.4
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
